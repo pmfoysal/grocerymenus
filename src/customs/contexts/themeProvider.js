76db7891538523bac53ca48da6308632b9b88363
pmfoysal <pmfoysal@gmail.com>
2022-04-29 11:16:00 +0600

    ThemeProvider Component Added

---
+++ src/customs/contexts/themeProvider.js
@@ -0,0 +1,19 @@
+import themes from 'customs/themes/themes';
+import React, {createContext, useState, useEffect} from 'react';
+import {ThemeProvider as StyledTheme} from 'styled-components';
+
+export const ThemeContext = createContext();
+
+export default function ThemeProvider({children}) {
+   const [theme, setTheme] = useState('white');
+
+   useEffect(() => {
+      window.localStorage.setItem('theme', theme);
+   }, [theme]);
+
+   return (
+      <ThemeContext.Provider value={{theme, setTheme}}>
+         <StyledTheme theme={themes[theme]}>{children}</StyledTheme>
+      </ThemeContext.Provider>
+   );
+}
