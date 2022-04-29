449fea81c2cf5795eeee544a735441a1fa87a75f
pmfoysal <pmfoysal@gmail.com>
2022-04-29 10:41:02 +0600

    index.js File Updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from '@app/app';
+import ReactDOM from 'react-dom';
+import '@commonStyle/reset.scss';
+import AppWrapper from '@helper/appWrapper';
+import reportWebVitals from '@test/reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.querySelector("[data-app='pmfoysal']"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+   <React.StrictMode>
+      <AppWrapper>
+         <App />
+      </AppWrapper>
+   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
