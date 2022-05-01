6c7b858720227758c4e0cf1a6995d416ff28e91c
pmfoysal <pmfoysal@gmail.com>
2022-05-01 12:11:56 +0600

    MongoDB Connection Added

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
+const {MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d3xoe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1});
+
 const corsOptions = {
    origin: '*',
    allowedHeaders: true,
@@ -12,6 +16,17 @@ const corsOptions = {
 
 app.use(express.json());
 app.use(cors(corsOptions));
+
+async function runDatabase() {
+   try {
+      await client.connect();
+      const products = client.db('pmphas11').collection('products');
+   } finally {
+      // await client.close();
+   }
+}
+
+runDatabase().catch(console.dir);
 
 app.get('/', (req, res) => {
    res.send('Hellow from PmPhAs11 Server!');
