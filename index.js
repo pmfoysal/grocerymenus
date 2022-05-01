5feb5a74e9ceb603ef484e4a814b195e664c0820
pmfoysal <pmfoysal@gmail.com>
2022-05-01 10:38:41 +0600

    .env File Added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require('express');
+require('dotenv').config();
 const cors = require('cors');
+const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 5000;
