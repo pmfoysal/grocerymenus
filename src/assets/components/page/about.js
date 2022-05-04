20945e5f2a4b55d86f327fce5c2b2f46520ba6fe
pmfoysal <pmfoysal@gmail.com>
2022-05-04 17:53:17 +0600

    About Page Component Added

--- src/assets/components/page/about.js
+++ src/assets/components/page/about.js
@@ -1,5 +1,62 @@
 import React from 'react';
+import {Icon} from '@iconify/react';
+import PageTitle from '@coreComp/pageTitle';
+import MainContainer from '@coreComp/mainContainer';
+import {
+   AboutCard,
+   AboutCardIcon,
+   AboutCardIcons,
+   AboutCardImage,
+   AboutCardLeft,
+   AboutCardName,
+   AboutCardPara,
+   AboutCardRole,
+   AboutContainer,
+} from '@pageStyle/about.styles';
 
 export default function About() {
-   return <h1 style={{fontSize: '3rem'}}>This is About Page!</h1>;
+   return (
+      <AboutContainer>
+         <MainContainer>
+            <PageTitle>
+               about <span>me</span>
+            </PageTitle>
+            <AboutCard>
+               <AboutCardLeft>
+                  <AboutCardImage src='https://avatars.githubusercontent.com/u/79983010?v=4' alt='site-author' />
+                  <AboutCardName>foysal ahmmed</AboutCardName>
+                  <AboutCardRole>react developer</AboutCardRole>
+                  <AboutCardIcons>
+                     <AboutCardIcon href='https://facebook.com/pmfoysalz' target='_blank'>
+                        <Icon icon='cib:facebook-f' />
+                     </AboutCardIcon>
+                     <AboutCardIcon href='https://linkedin.com/in/pmfoysal' target='_blank'>
+                        <Icon icon='cib:linkedin-in' />
+                     </AboutCardIcon>
+                     <AboutCardIcon href='https://instagram.com/pmfoysal' target='_blank'>
+                        <Icon icon='cib:instagram' />
+                     </AboutCardIcon>
+                     <AboutCardIcon href='https://twitter.com/pmfoysal' target='_blank'>
+                        <Icon icon='cib:twitter' />
+                     </AboutCardIcon>
+                     <AboutCardIcon href='https://github.com/pmfoysal' target='_blank'>
+                        <Icon icon='cib:github' />
+                     </AboutCardIcon>
+                  </AboutCardIcons>
+               </AboutCardLeft>
+               <AboutCardPara>
+                  <span className='title'>about me</span>
+                  <br />
+                  Hi, I am <span className='name'>Foysal Ahmmed</span>, a Bengali based Web Developer & Designer. Currently I am
+                  focusing on Full Stack & MERN Stack Development. I have completed 12 years of education based on Science
+                  Depertment. Last 1.5 years I have learned many Languages, Tools & Technologies like HTML, CSS, JavaScript,
+                  ReactJS, NodeJS, ExpressJS, MongoDB, GitHub, Netlify, Yarn, NPM, Visual Studio Code etc. I have completed 55+
+                  projects with the help of these Technologies. I have 1.5+ years of Coding Experience with Bug Fixing. I have few
+                  knowledge about Java, Android Studio, NextJS, ReduxJS. I am an experienced Web Development Specialist with a
+                  demonstrated history of working in the Computer Software Industry!
+               </AboutCardPara>
+            </AboutCard>
+         </MainContainer>
+      </AboutContainer>
+   );
 }
--- src/assets/styles/page/about.styles.js
+++ src/assets/styles/page/about.styles.js
@@ -1,1 +1,127 @@
 import styled from 'styled-components';
+
+export const AboutContainer = styled.section`
+   background-color: ${({theme}) => theme.back.card0};
+   padding-bottom: 8rem;
+`;
+
+export const AboutCard = styled.article`
+   width: 120rem;
+   margin: 0 auto;
+   padding: 4rem;
+   border-radius: 2rem;
+   background-color: ${({theme}) => theme.back.page};
+   box-shadow: 0 0 1.5rem rgba(0, 0, 0, 0.1);
+   display: flex;
+   align-items: center;
+   gap: 4rem;
+`;
+
+export const AboutCardLeft = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+   padding-right: 4rem;
+
+   &::after {
+      content: '';
+      width: 0.3rem;
+      height: 60%;
+      position: absolute;
+      top: 50%;
+      right: 0;
+      transform: translateY(-50%);
+      background-color: ${({theme}) => theme.back.card2};
+   }
+`;
+
+export const AboutCardImage = styled.img`
+   height: 17.5rem;
+   width: 17.5rem;
+   border-radius: 100%;
+`;
+
+export const AboutCardName = styled.h1`
+   font-size: 2.2rem;
+   color: ${({theme}) => theme.text.main.head};
+   white-space: nowrap;
+   margin-top: 2rem;
+`;
+
+export const AboutCardRole = styled.h3`
+   font-size: 1.5rem;
+   font-weight: 550;
+   color: ${({theme}) => theme.back.accent};
+   margin-top: 0.25rem;
+`;
+
+export const AboutCardIcons = styled.div`
+   width: 100%;
+   display: flex;
+   align-items: center;
+   gap: 1.5rem;
+`;
+
+export const AboutCardIcon = styled.a`
+   margin-top: 2rem;
+   height: 3.5rem;
+   width: 3.5rem;
+   border-radius: 100%;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   background-color: ${({theme}) => theme.back.card1};
+   transition: 0.1s ease;
+
+   svg {
+      color: ${({theme}) => theme.back.accent};
+      transition: 0.1s ease;
+      height: 1.8rem;
+      width: auto;
+   }
+
+   &:hover {
+      background-color: ${({theme}) => theme.back.accent};
+
+      svg {
+         color: ${({theme}) => theme.back.page};
+      }
+   }
+
+   &:active {
+      transform: scale(0.95);
+   }
+`;
+
+export const AboutCardPara = styled.p`
+   flex-grow: 1;
+   color: ${({theme}) => theme.text.main.para};
+   line-height: 1.6;
+
+   .title {
+      font-size: 2rem;
+      font-weight: 700;
+      text-transform: capitalize;
+      line-height: 1;
+      margin-bottom: 1.5rem;
+
+      &::after {
+         content: '';
+         position: absolute;
+         top: 50%;
+         transform: translateY(-50%);
+         left: calc(100% + 1rem);
+         height: 0.2rem;
+         width: 20rem;
+         background-color: ${({theme}) => theme.back.card2};
+      }
+   }
+
+   .name {
+      font-weight: 700;
+      line-height: inherit;
+      font-size: inherit;
+      color: inherit;
+   }
+`;
