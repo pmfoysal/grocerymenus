bb0b4bc3a7860e339b9cc39ae8588e6550e17a18
pmfoysal <pmfoysal@gmail.com>
2022-05-05 11:56:20 +0600

    Signup Page Component Added

--- src/assets/components/page/signup.js
+++ src/assets/components/page/signup.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import image from '@image/authbg.jpg';
+import PageTitle from '@coreComp/pageTitle';
+import MainContainer from '@coreComp/mainContainer';
+import {SignupContainer, SignupContent, SignupForm, SignupTitle, SignupHead, SignupPara} from '@pageStyle/signup.styles';
+import {Link} from 'react-router-dom';
 
 export default function Signup() {
-   return <h1 style={{fontSize: '3rem'}}>This is Signup Page!</h1>;
+   return (
+      <SignupContainer>
+         <MainContainer>
+            <PageTitle>
+               new <span>account</span>
+            </PageTitle>
+            <SignupContent>
+               <img src={image} alt='background' />
+               <SignupForm>
+                  <SignupTitle>hey, good morning!</SignupTitle>
+                  <SignupHead>Create an Account</SignupHead>
+                  <SignupPara>
+                     Already have an account? <Link to='/signin'>Signin</Link>
+                  </SignupPara>
+               </SignupForm>
+            </SignupContent>
+         </MainContainer>
+      </SignupContainer>
+   );
 }
--- src/assets/styles/page/signup.styles.js
+++ src/assets/styles/page/signup.styles.js
@@ -1,1 +1,80 @@
 import styled from 'styled-components';
+
+export const SignupContainer = styled.section`
+   background-color: ${({theme}) => theme.back.card0};
+   padding-bottom: 8rem;
+`;
+
+export const SignupContent = styled.article`
+   padding: 8rem 0;
+   border-radius: 3rem;
+   overflow: hidden;
+
+   &::after {
+      content: '';
+      height: 100%;
+      width: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      background-color: rgba(0, 0, 0, 0.1);
+   }
+
+   & > img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      position: absolute;
+      top: 0;
+      left: 0;
+   }
+`;
+
+export const SignupForm = styled.div`
+   width: 50rem;
+   padding: 3rem;
+   background-color: rgba(255, 255, 255, 0.75);
+   backdrop-filter: blur(0.75rem);
+   margin: 0 auto;
+   box-shadow: 0 0 1.25rem rgba(0, 0, 0, 0.1);
+   border-radius: 2rem;
+   z-index: 2;
+`;
+
+export const SignupHead = styled.h1`
+   font-size: 3rem;
+   text-transform: none;
+   text-align: center;
+   line-height: 1;
+   color: ${({theme}) => theme.text.main.head};
+`;
+
+export const SignupTitle = styled.p`
+   font-size: 2rem;
+   text-transform: capitalize;
+   text-align: center;
+   width: 100%;
+   margin-bottom: 0.5rem;
+   color: ${({theme}) => theme.text.main.para};
+`;
+
+export const SignupPara = styled.p`
+   text-align: center;
+   width: 100%;
+   margin-top: 1rem;
+   color: ${({theme}) => theme.text.main.para};
+
+   a {
+      color: ${({theme}) => theme.back.accent};
+      font-weight: 550;
+      transition: 0.1s ease;
+
+      &:hover {
+         opacity: 0.85;
+      }
+
+      &:active {
+         transform: scale(0.95);
+      }
+   }
+`;
