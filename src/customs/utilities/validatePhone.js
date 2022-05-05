036261e839d1d055841866db63621b0266b9daf6
pmfoysal <pmfoysal@gmail.com>
2022-05-05 17:42:08 +0600

    Phone Validator Added

---
+++ src/customs/utilities/validatePhone.js
@@ -0,0 +1,16 @@
+import {toast} from 'react-toastify';
+
+export default function validatePhone(text) {
+   if (!text) {
+      toast.error(`Please provide your phone number!`, {autoClose: 3000});
+      return false;
+   }
+   const newText = text.replaceAll(/[ -+]+/g, '');
+   for (const char of newText) {
+      if (!isNaN(char)) {
+         toast.error('Phone number should only be a Number!', {autoClose: 3000});
+         return false;
+      }
+   }
+   return true;
+}
