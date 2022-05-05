1605957da1aa7b7835fced49fc57ae6d8a86ff33
pmfoysal <pmfoysal@gmail.com>
2022-05-05 15:18:29 +0600

    Forget Page Component Added

--- src/assets/components/page/forget.js
+++ src/assets/components/page/forget.js
@@ -1,5 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
+import InputBox from '@coreComp/inputBox';
+import PageTitle from '@coreComp/pageTitle';
+import MainContainer from '@coreComp/mainContainer';
+import {
+   SignupContainer,
+   SignupContent,
+   SignupForm,
+   SignupTitle,
+   SignupHead,
+   SignupInputArea,
+   SignupButton,
+} from '@pageStyle/signup.styles';
 
 export default function Forget() {
-   return <h1 style={{fontSize: '3rem'}}>This is Forget Page!</h1>;
+   const [firstName, setFirstName] = useState('');
+
+   function inputHandler(setter) {
+      return event => {
+         const value = event.target.value;
+         event.target.setAttribute('data-text', Boolean(value));
+         setter(value);
+      };
+   }
+
+   return (
+      <SignupContainer>
+         <MainContainer>
+            <PageTitle>
+               forget <span>password</span>
+            </PageTitle>
+            <SignupContent>
+               <SignupForm>
+                  <SignupTitle>hey, don't worry!</SignupTitle>
+                  <SignupHead>Reset your Password</SignupHead>
+                  <SignupInputArea>
+                     <InputBox name='your email' type='email' handler={inputHandler(setFirstName)} />
+                  </SignupInputArea>
+                  <SignupButton>send reset email</SignupButton>
+               </SignupForm>
+            </SignupContent>
+         </MainContainer>
+      </SignupContainer>
+   );
 }
