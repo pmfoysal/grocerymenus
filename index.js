bc9867bc96040f4bba1893fc93c81325c4660cf3
pmfoysal <pmfoysal@gmail.com>
2022-05-06 20:11:47 +0600

    Cors Issues Fixed

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1});
 
 const corsOptions = {
-   origin: ['*', 'https://localhost:3000'],
-   allowedHeaders: true,
-   credentials: true,
+   origin: '*',
 };
 
 app.use(express.json());
