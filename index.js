6c4e10c74872e23dd869c068ca17e94779a76b0c
pmfoysal <pmfoysal@gmail.com>
2022-05-06 19:06:02 +0600

    Server Issues Fixed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1});
 
 const corsOptions = {
-   origin: '*',
+   origin: ['*'],
    allowedHeaders: true,
    credentials: true,
 };
