68b5e268555c97bbe49c277621f228290b5ae5b1
pmfoysal <pmfoysal@gmail.com>
2022-05-08 10:02:33 +0600

    EmailPass Signin Token Added

--- src/customs/auth/emailPassSignin.js
+++ src/customs/auth/emailPassSignin.js
@@ -1,3 +1,4 @@
+import getToken from './getToken';
 import {toast} from 'react-toastify';
 import {getAuth, signInWithEmailAndPassword} from 'firebase/auth';
 
@@ -7,14 +8,11 @@ export default function emailPassSignin(data) {
    const {email, password} = data;
    const id = toast.loading('Wait... Connecting to the server!');
    signInWithEmailAndPassword(auth, email, password)
-      .then(() => {
-         const options = {
-            render: 'You have successfully signed in!',
-            type: 'success',
-            isLoading: false,
-            autoClose: 3000,
-         };
-         toast.update(id, options);
+      .then(result => {
+         toast.update(id, {
+            render: 'Signed in! Now getting access token...!',
+         });
+         getToken(result, id);
       })
       .catch(error => {
          const options = {
